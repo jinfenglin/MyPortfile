752bc4698bda58c823f28678a9a63d52cda51400
jinfenglin <ljfnwpusife@gmail.com>
2016-04-04 20:45:36 -0400

    struggle with draggable

--- Portfile/static/js/masterpiece_editor.js
+++ Portfile/static/js/masterpiece_editor.js
@@ -1,16 +1,5 @@
 var fileCache={};
-function createTextInput(){
-	var newText=document.createElement("input");
-	var hinter=document.getElementById("hinter");
-	var br= document.createElement("br");
-	//newText=newText.appendChild(br);
-	newText.type="text";
-	last=document.body.lastChild;
-	hinter.innerHTML=last.innerHTML;
-	document.body.insertBefore(newText,last);
-	document.body.insertBefore(br,last);	
-	//document.body.appendChild(newText);
-}
+var blocks=[];
 window.onload=function(){
 	if(window.File && window.FileList && window.FileReader){
 		Init();
@@ -29,9 +18,10 @@ function Init(){
 		filedrag.addEventListener("dragleave", FileDragHover, false);
 		filedrag.addEventListener("drop", FileSelectHandler, false);
 		filedrag.style.display = "block";
-
 	}	
 }
+
+
 function FileDragHover(e){
 	e.stopPropagation();
 	e.preventDefault();
@@ -48,7 +38,50 @@ function FileSelectHandler(e){
 	console.log("file cacahed="+Object.keys(fileCache).length);
 	
 }
-function MySubmission(evt){
+
+function TextBlock(){
+	this.text="";
+	this.width="200px";
+	var frame=document.createElement('div');
+	var container=document.createElement('div');
+	var textArea=document.createElement('textarea');
+	textArea.style.width=this.width;
+	textArea.style.height=this.height;
+
+	document.body.appendChild(frame);
+	frame.appendChild(container);
+	frame.appendChild(document.createElement('textarea'));
+	container.appendChild(textArea);
+	
+	textArea.addEventListener("keyup",storeText,false);
+	textArea.setAttribute('class','moveable-textarea');
+	$(container).draggable({cancel:false,containment:"parent"});
+	$(textArea).resizable({cancel:false});
+
+	container.style.border='1px solid #F00';
+	frame.style.border='1px solid #F00';
+	container.addEventListener('click',function(){
+		textArea.focus();
+	});
+
+	
+	//$(textArea).resizable({cancel:''});
+
+	function storeText(){
+		this.text=textArea.value;
+		console.log(this.text);
+	}
+	
+}
+function VideoBlock(){//create video upload area# show video files  icon in the area
+			    //provide function to delete the video(mouse or key)
+	return null;
+}
+function ImageBlock(){//create image upload area #show image in place, provide function to delete the image
+	return null;
+}
+
+function MySubmission(evt){//rewrite this function, press to save the data to server
 	console.log("start sending...");
 	//evt.preventDefault();
 	var xhr=new XMLHttpRequest();
@@ -61,7 +94,5 @@ function MySubmission(evt){
 		xhr.open("POST","masterpiece_edit",true);
 		xhr.send(formData);
 	}
-
-
 }
 	
