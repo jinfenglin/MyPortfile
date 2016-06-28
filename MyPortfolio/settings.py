a2d938d0da5c5c169d2e3310e0b72e765cd8c657
jinfenglin <ljfnwpusife@gmail.com>
2016-06-27 23:34:01 -0400

    base

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT='static'
 MEDIA_ROOT='Portfolio/media'
