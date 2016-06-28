f93daebfa86001f91036bace598ea6ee4b0e029a
jinfenglin <ljfnwpusife@gmail.com>
2016-06-28 00:05:03 -0400

    can't load static

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -146,7 +146,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_DIRS=[
+    os.path.join(BASE_DIR,'static/shared/img')
+]
 
 STATIC_URL = '/static/'
 STATIC_ROOT='static'
