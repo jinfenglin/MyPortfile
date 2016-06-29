3aa01791577ad68907ebdbb9daa5d5a8d0043845
jinfenglin <ljfnwpusife@gmail.com>
2016-06-29 09:47:58 -0400

    merge

---
+++ Portfolio/media/user_jinfenglin/<class 'RepoKeeper.models.Recourse'>/test.java
@@ -0,0 +1,8 @@
+class Test{
+	Test(){
+		System.out.println("hello");
+	}
+	public static void main(String[]  args){
+		Test t=new Test();
+	}
+}
--- RepoKeeper/urls.py
+++ RepoKeeper/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$',ManagePage.as_view(),name='manage')
+    url(r'^$',ManagePage.as_view(),name='manage'),
+    url(r'^uploadPage',Upload.as_view(),name='upload'),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
