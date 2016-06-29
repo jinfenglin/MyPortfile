48aa0506419c3ac38e7950f72225bbad37da21b2
jinfenglin <ljfnwpusife@gmail.com>
2016-06-28 20:24:57 -0400

    for pull

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_DIRS=[
-    os.path.join(BASE_DIR,'static/shared/img')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static/shared/')
 ]
 
 STATIC_URL = '/static/'
