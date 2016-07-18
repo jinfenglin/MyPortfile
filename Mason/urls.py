48a99bdbf1b4104f50f6c2ec7f0264127f10d378
jinfenglin <ljfnwpusife@gmail.com>
2016-07-17 22:17:49 -0400

    remove the side bar icon from base to managerpage

--- Mason/urls.py
+++ Mason/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns =[
-        url(r'^$',HomePage.as_view(),name='editor'),
+        url(r'^$',Editer.as_view(),name='editor'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Mason/views.py
+++ Mason/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 # Create your views here.
-class HomePage(View):
-    def get(self, *args):
-        return None;
+class Editer(View):
+    def get(self, request):
+        return render(request, 'Mason/editor.html');
